a7f189e7c5c364a31af5c60a0cc69a477b5561c3
osama <osamamelek21@gmail.com>
2025-05-27 17:58:03 +0300

    updating main files

--- app.js
+++ app.js
@@ -15,3 +15,33 @@ const viewCart = () => {
     list[1].style.display = 'none' ;
     list[2].style.display = 'block' ;
 }
+
+// cart function snd variables  
+let totalItemsPrice = 0 ;
+let totalShipingPrice = 0 ;
+
+let productsList = document.getElementById('cart-products-list');
+function Add( itemName , price , imgSrc , id ) {
+    let newDiv = document.createElement("div");
+    productsList.appendChild(newDiv);
+    newDiv.setAttribute("id", id );
+    newDiv.setAttribute("class", "product");
+    newDiv.innerHTML = `
+        <img src="${imgSrc}" width="90%">
+        <p class="item-name"> ${itemName} </p>
+        <p class="item-price"> Price : <b> ${price} $</b> </p> 
+        <button onclick="removefunc('${id}')"> Remove </button> <br>
+    `;
+    totalItemsPrice += Number(price) ;
+    totalShipingPrice += 10 ;
+    let totalPrice =  totalItemsPrice + totalShipingPrice ;
+    document.getElementById('subtotal').textContent = totalItemsPrice ;
+    document.getElementById('shiping').textContent = totalShipingPrice ;
+    document.getElementById('total').textContent = totalPrice ;
+    
+
+}
+const removefunc = (itemId) => {
+    let item = document.getElementById( itemId );
+    item.remove() ;
+}
